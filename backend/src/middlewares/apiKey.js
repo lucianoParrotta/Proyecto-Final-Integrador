e572b28718043a724ceb9776d26084d68af466b1
FedeRuppel <federupel@gmail.com>
2025-11-28 18:43:53 +0000

    SGIG-15: defini /stats/dashboard, agregue consultas SQL necesarias, actualice ApiKey para aceptar dos claves (general y stats), agregue coleccion y environment de postman

--- backend/src/middlewares/apiKey.js
+++ backend/src/middlewares/apiKey.js
@@ -1,7 +1,12 @@
 function apiKeyMiddleware(req, res, next) {
   const apiKey = req.headers['x-api-key'];
 
-  if (!apiKey || apiKey !== process.env.API_KEY) {
+  const validKeys = [
+    process.env.API_KEY,       // clave general
+    process.env.API_KEY_STATS  // clave exclusiva para /stats 
+  ].filter(Boolean); // elimina undefined
+
+  if (!apiKey || !validKeys.includes(apiKey)) {
     return res.status(401).json({ error: "API Key inválida" });
   }
 
